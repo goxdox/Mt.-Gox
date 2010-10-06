1f1851a012604a9b23264f28cf4a6012d8dff879
jed_vaio <jed@mtgox.com>
2010-10-06 16:19:18 -0400

    .

--- app/webroot/code/gateway/checkTxn.php
+++ app/webroot/code/gateway/checkTxn.php
@@ -1,19 +1,17 @@
 <?php
 include('../../../noserve/config.inc');
 include('../lib/functions.inc');
-
+include('../lib/common.inc');
 //echo(print_r($_POST));
 
 db_connect();
 
 $merchID=(int)$_REQUEST['merchID'];
-$amount=round((float)$_REQUEST['amount'],4); 
+$amount=(float)$_REQUEST['amount']; 
 $txn_id=mysql_real_escape_string($_REQUEST['txn_id']);
 
 if($merchID && $amount && $txn_id)
 {
-	
-	
 	$sql="SELECT Amount from MerchantOrders where txn_id='$txn_id' and status=1 and MerchantID=$merchID";
 	$dbAmount=round(getSingleDBValue($sql)/BASIS,2);
 	if($dbAmount==$amount) 
