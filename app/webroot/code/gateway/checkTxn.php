4930e4b2c7b7cd8f1b1976e334bb276d500ef3d1
jed_vaio <jed@mtgox.com>
2010-10-06 16:15:31 -0400

    bug

--- app/webroot/code/gateway/checkTxn.php
+++ app/webroot/code/gateway/checkTxn.php
@@ -4,17 +4,18 @@ include('../lib/functions.inc');
 
 //echo(print_r($_POST));
 
-	
+db_connect();
+
 $merchID=(int)$_REQUEST['merchID'];
 $amount=round((float)$_REQUEST['amount'],4); 
 $txn_id=mysql_real_escape_string($_REQUEST['txn_id']);
 
 if($merchID && $amount && $txn_id)
 {
-	db_connect();
+	
 	
 	$sql="SELECT Amount from MerchantOrders where txn_id='$txn_id' and status=1 and MerchantID=$merchID";
-	$dbAmount=round(getSingleDBValue($sql),4);
+	$dbAmount=round(getSingleDBValue($sql)/BASIS,2);
 	if($dbAmount==$amount) 
 	{
 		echo("ok");
