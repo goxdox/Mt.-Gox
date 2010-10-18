4f54a190c0b4d2e33633c89d1d8213b4fa440bc4
jed_vaio <jed@mtgox.com>
2010-10-18 16:31:15 -0400

    liqpay

---
+++ app/webroot/code/liqpay/test.php
@@ -0,0 +1,43 @@
+<?
+include('../../../noserve/config.inc');
+include('../lib/functions.inc');
+include('../lib/session.php');
+include('../lib/common.inc');
+
+global $LIQPAY_MERCHANT_ID;
+global	$LIQPAY_SIG;
+
+
+
+$url="https://www.liqpay.com/?do=clickNbuy";
+$method='card';
+$phone='+20123145121';
+
+	$xml="<request>      
+		<version>1.2</version>
+        <action>send_money</action>
+		<result_url>http://mysite.com/lqanswer.php</result_url>
+		<server_url>http://mysite.com/lqanswer.php</server_url>
+		<merchant_id>$LIQPAY_MERCHANT_ID</merchant_id>
+		<order_id>ORDER_1234</order_id>
+		<amount>10</amount>
+		<currency>USD</currency>
+		<description>Description</description>
+		<default_phone>13472565876</default_phone>
+		<pay_way>card</pay_way> 
+		</request>
+		";
+	
+	
+	$xml_encoded = base64_encode($xml); 
+	$lqsignature = base64_encode(sha1($LIQPAY_SIG.$xml.$LIQPAY_SIG,1));
+	
+
+
+echo("<form action='$url' method='POST'>
+      <input type='hidden' name='operation_xml' value='$xml_encoded' />
+      <input type='hidden' name='signature' value='$lqsignature' />
+	<input type='submit' value='Pay'/>
+	</form>");
+?>
+	
