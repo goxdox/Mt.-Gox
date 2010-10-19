073d8665f9c3f042fb033badb7cd504f1804a0dc
jed_vaio <jed@mtgox.com>
2010-10-19 08:50:56 -0400

    .

--- app/webroot/code/liqpay/checkout.php
+++ app/webroot/code/liqpay/checkout.php
@@ -49,7 +49,7 @@ if(isset($_SESSION['UserID']))
 	$result['error'] ="Not logged in.";
 }
 
-echo("<form action='$url' method='POST'>
+echo("<form action='https://www.liqpay.com/?do=clickNbuy' method='POST'>
       <input type='hidden' name='operation_xml' value='$xml_encoded' />
       <input type='hidden' name='signature' value='$lqsignature' />
 	<input type='submit' value='Pay'/>
