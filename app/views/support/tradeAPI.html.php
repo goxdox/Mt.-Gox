9b404be21730d3510ee21a60576d03e5eed1c5a8
jed_vaio <jed@mtgox.com>
2010-11-08 16:36:14 -0500

    .

--- app/views/support/tradeAPI.html.php
+++ app/views/support/tradeAPI.html.php
@@ -14,10 +14,10 @@ The following take your Mt Gox username and password as parameters. They must be
 <table class="btcx_table" >
 <thead><tr><th colspan=2>Placing Orders</th></tr></thead>
 <tr><td>Get your current balance</td><td><b>https://mtgox.com/code/getFunds.php?name=blah&pass=blah</b></td></tr>
-<tr><td>Place an order to Buy BTC</td><td><b>https://mtgox.com/code/buyBTC.php?name=blah&pass=blah&amount=#price=#</b><br>returns a list of your open orders</td></tr>
-<tr><td>Place an order to Sell BTC</td><td><b>https://mtgox.com/code/sellBTC.php?name=blah&pass=blah&amount=#price=#</b<br>returns a list of your open orders</td></tr>
+<tr><td>Place an order to Buy BTC</td><td><b>https://mtgox.com/code/buyBTC.php?name=blah&pass=blah&amount=#&price=#</b><br>returns a list of your open orders</td></tr>
+<tr><td>Place an order to Sell BTC</td><td><b>https://mtgox.com/code/sellBTC.php?name=blah&pass=blah&amount=#&price=#</b<br>returns a list of your open orders</td></tr>
 <tr><td>Fetch a list of your open Orders</td><td><b>https://mtgox.com/code/getOrders.php?name=blah&pass=blah</b<br>oid: Order ID<br>type: 1 for sell order or 2 for buy order<br>status: 1 for active, 2 for not enough funds</td></tr>
-<tr><td>Cancel an order</td><td><b>https://mtgox.com/code/sellBTC.php?name=blah&pass=blah&oid=#type=#</b<br>oid: Order ID<br>type: 1 for sell order or 2 for buy order</td></tr>
+<tr><td>Cancel an order</td><td><b>https://mtgox.com/code/sellBTC.php?name=blah&pass=blah&oid=#&type=#</b<br>oid: Order ID<br>type: 1 for sell order or 2 for buy order</td></tr>
 </table>
 <p>
 That's it. Now write a bot!
