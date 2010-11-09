de0f056c1cb043fe9eb9abf139590c24db34e2d9
jed_vaio <jed@mtgox.com>
2010-11-09 13:21:20 -0500

    .

---
+++ app/views/elements/footer.html.php
@@ -0,0 +1,12 @@
+<div id="footer" class="container" >
+       <HR style="margin-top: 30px;">
+      
+	<div id="etc">
+		
+		<a href="/support/tradeAPI">Trade API</a>
+		<a href="/merch/about">Merchant Services</a>
+		<a href="/support/contact">Contact</a>
+	</div>
+	<div id="legal">&copy; 2010 Pongamia Cultivation Institute, SA</div>
+
+    </div>
