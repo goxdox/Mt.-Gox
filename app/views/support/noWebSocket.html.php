04b69dc1c5032f2191263f6af0d38fd4f260d83b
jed_vaio <jed@mtgox.com>
2010-11-10 10:51:03 -0500

    .

---
+++ app/views/support/noWebSocket.html.php
@@ -0,0 +1,8 @@
+
+<h1>Sorry this page requires WebSocket support from your browser.</h1>
+Browsers that support WebSocket include:
+<ul>
+<li><a href="http://www.google.com/chrome/intl/en/landing_chrome.html?hl=en" target="_blank">Google Chrome</a></li>
+<li>Safari</li>
+<li> Alpha versions of FireFox</li>
+</ul>
