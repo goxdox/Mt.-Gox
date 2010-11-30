a6cbb8f0de497ef6d21410537d4083ac7ba7d39c
jed_vaio <jed@mtgox.com>
2010-11-30 14:55:29 -0500

    .

--- rts/SubscribePool.py
+++ rts/SubscribePool.py
@@ -123,7 +123,7 @@ class SubscribePool():
             
             index=0
             for row in rows:
-                self.mData['plot'][index]={row['price'],0,0,0, round(row['amount']/1000,0), row['date'] }
+                self.mData['plot'][index]={0:row['price'],1:0,2:0,3:0, 4:round(row['amount']/1000,0), 5:row['date'] }
                 index =index+1
                       
         except MySQLdb.Error, e:
